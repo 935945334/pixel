2311a2b752923b567d568f78d264f78c099920ec
935945334 <935945334@qq.com>
2020-02-15 03:15:36 +0800

    Update pixel-move.js

--- pixel-move.js
+++ pixel-move.js
@@ -24,6 +24,7 @@ function roleHeight(){
     // role.style.marginTop = "70px";
     role.style.marginTop = kt.offsetHeight - 60 + "px"
     role.style.paddingTop = 70 + "px"
+    role.style.paddingLeft = "43vh"
     // role.style.paddingLeft = kt.offsetWidth/2 - 30 + "px"
     // role.style.marginLeft = - 30 + "px"
     // alert(kt.offsetHeight);
@@ -55,8 +56,8 @@ function move(){
         var clientY = m_clientY;
         window.clientX = clientX;
         // window.clientX = clientX -70; 
-        // window.clientY = clientY - parseInt(kt.offsetWidth/2);
-        window.clientY = clientY;  
+        window.clientY = clientY - parseInt(kt.offsetWidth/2);
+        // window.clientY = clientY;  
     }
     roleX()
     var i=0;
