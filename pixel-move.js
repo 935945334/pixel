4c46ab407d6edb3872e978bbc29fc8acc4a787e2
935945334 <935945334@qq.com>
2020-02-15 04:00:12 +0800

    Update pixel-move.js

--- pixel-move.js
+++ pixel-move.js
@@ -56,7 +56,7 @@ function move(){
         var clientY = m_clientY;
         window.clientX = clientX;
         // window.clientX = clientX -70; 
-        window.clientY = clientY - parseInt(kt.offsetWidth/2) - 100;
+        window.clientY = clientY - parseInt(kt.offsetWidth/2) + 70;
         // window.clientY = clientY;
     }
     roleX()
