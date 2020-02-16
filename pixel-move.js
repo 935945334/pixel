55cc340f8c89c5186c0e758147b67bf47aade489
935945334 <935945334@qq.com>
2020-02-16 16:20:22 +0800

    Update pixel-move.js

--- pixel-move.js
+++ pixel-move.js
@@ -66,12 +66,37 @@ function roleHeight(){
     // alert(kt.offsetHeight);
 }
 function get_canvas(ev, obj) {
+    // if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)) { 
+    //     // alert("苹果");
+    //     if (window.orientation == 90 || window.orientation == -90) {
+    //         // alert("苹果");
+    //         // alert("横屏");
+    //     }else if(window.orientation == 180 || window.orientation == 0){
+    //         // alert("苹果");
+    //         // alert("竖屏");
+    //     }
+    // } else if (/(Android)/i.test(navigator.userAgent)) { 
+    //     // alert("安卓");
+    //     if (window.orientation == 90 || window.orientation == -90) {
+    //         // alert("安卓");
+    //         // alert("横屏");
+    //         m_clientX = ev.clientX - obj.offsetLeft;
+    //         m_clientY = ev.clientY - obj.offsetTop;
+    //     }else if(window.orientation == 180 || window.orientation == 0) {
+    //         // alert("安卓");
+    //         // alert("竖屏");
+    //         m_clientX = ev.clientX - kb.offsetHeight;
+    //         m_clientY = ev.clientY - kl.offsetWidth;
+    //     } 
+    // } else {
+    //     // alert("电脑");
+    };
     if (/(iPhone|iPad|iPod|iOS|Android)/i.test(navigator.userAgent)) {
         // alert("手机")
         if (window.orientation == 90 || window.orientation == -90) {
             // alert("横屏");
-            m_clientX = ev.clientX - obj.offsetLeft;
-            m_clientY = ev.clientY - obj.offsetTop;
+            m_clientX = ev.clientX - kl.offsetWidth;
+            m_clientY = ev.clientY - kb.offsetHeight;
         }else if(window.orientation == 180 || window.orientation == 0) {
             // alert("竖屏");
             m_clientX = ev.clientX - kb.offsetHeight;
