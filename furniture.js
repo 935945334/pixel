423baa4c3f3871a277040493d89d2ed8a501079d
935945334 <935945334@qq.com>
2020-02-16 21:36:05 +0800

    1

--- furniture.js
+++ furniture.js
@@ -15,6 +15,7 @@ function Mobile(){
 	}else if(window.orientation == 90 || window.orientation == -90){
         //横屏
         play.className = "play-Mobile-y";
+        play.style.height = screen.height + "px";
         kl.style.height = "100vh";
         kr.style.height = "100vh";
         }
